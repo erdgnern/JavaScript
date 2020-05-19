9393fdedf100ea41ebe6e8da793a47ad710b7722
erdgnern <eren.erdogan@yahoo.com>
2020-05-19 01:58:53 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Burada name parameter, greet'in icindeki isim de arguement. Arguement sundugumuz value, parameter ise declarationda kullandigimiz oge
+
+// Performin a task
+function greet(firstName, lastName) {
+    console.log('Hello' + ' ' + firstName + ' ' + lastName);
+}
+
+greet('John', 'Smith');
+
+// Calculating a value
+
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2))
